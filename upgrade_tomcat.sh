a0b2a492f3b0ba4c3aac397b65fec197b893318e
Amrutanshu Dash <68193810+AmrutanshuDash@users.noreply.github.com>
2024-10-21 12:01:49 +0530

    Update upgrade_tomcat.sh

--- upgrade_tomcat.sh
+++ upgrade_tomcat.sh
@@ -10,11 +10,11 @@ wget https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.95/bin/apache-tomcat-9
 if [ $? -eq 0 ]; then
     echo "Download completed. Extracting the tar.gz file..."
     tar -zxvf apache-tomcat-9.0.95.tar.gz
-    
+    cd "$TOMCAT_DIR"
     unlink rel 2>/dev/null #if it exists
 
     # creating new symlink to the new version
-    ls -sf "$TOMCAT_DIR"/apache-tomcat-9.0.95
+    ls -sf "$TOMCAT_DIR"/apache-tomcat-9.0.95 rel
     echo "Tomcat installation completed successfully."
 else
     echo "Error downloading Tomcat."
